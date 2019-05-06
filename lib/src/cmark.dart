b7cdc4b02d3c160d51c34bc82b487d75c208880e
Manabu Nakazawa <mshibanami+git@gmail.com>
2019-05-06 21:24:31 +1000

    bit operation for options

--- lib/src/cmark.dart
+++ lib/src/cmark.dart
@@ -6,8 +6,11 @@ class CMark {
   String markdownToHtml(String text,
       {List<int> options = const [CMarkOptions.DEFAULT],
       List<int> extensions = const []}) {
+    var optionValue = options.fold(
+        CMarkOptions.DEFAULT, (previous, option) => previous | option);
     var textC = CString.allocate(text);
-    var renderedC = bindings.cmark_markdown_to_html(textC, text.length, 0);
+    var renderedC =
+        bindings.cmark_markdown_to_html(textC, text.length, optionValue);
     var rendered = CString.fromUtf8(renderedC);
     textC.free();
     renderedC.free();
